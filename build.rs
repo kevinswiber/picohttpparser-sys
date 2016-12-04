8e27c173c68f584bfa02c96d0dee0084c5a94570
Kevin Swiber <kswiber@gmail.com>
2016-12-04 13:00:20 -0800

    Adding compilation conditional for SSE4 support.

--- build.rs
+++ build.rs
@@ -1,11 +1,16 @@
+#![feature(cfg_target_feature)]
 extern crate gcc;
 
 fn main() {
-    gcc::Config::new()
-        .file("deps/picohttpparser/picohttpparser.c")
-        .include("deps/picohttpparser")
-        .flag("-msse4")
-        .compile("libpicohttpparser.a");
+    let mut config = gcc::Config::new();
+    config.file("deps/picohttpparser/picohttpparser.c");
+    config.include("deps/picohttpparser");
+
+    if cfg!(target_feature = "sse4.1") || cfg!(target_feature = "sse4.2") {
+        config.flag("-msse4");
+    }
+
+    config.compile("libpicohttpparser.a");
 
     println!("cargo:rustc-link-search=native={}",
              env!("CARGO_MANIFEST_DIR"));
